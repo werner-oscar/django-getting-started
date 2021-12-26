ac3d20fc0898082d0dca8ac8f7c88b7f7c47f6ac
werner-oscar <wernertsekumah@amalitech.org>
2021-12-26 16:54:11 +0000

    commit all django files

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from meetings.models import Meeting
+
+# Create your views here.
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+    {"num_meetings": Meeting.objects.count()})
+
+def date(request):
+    return HttpResponse("This page was generated at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("I am Hellscream!")
+
+
+
+
