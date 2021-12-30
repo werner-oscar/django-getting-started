8106cb724b736b81c579e10440b2f99ff459a0ea
werner-oscar <wernertsekumah@amalitech.org>
2021-12-30 12:53:11 +0000

    user interaction, ModelForms

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
     path('rooms', views.rooms_list, name="rooms"),
+    path('new', views.new, name="new"),
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 from .models import Meeting, Room
 # Create your views here.
@@ -11,3 +12,15 @@ def rooms_list(request):
     return render(request, "meetings/rooms_list.html",
             {"rooms": Room.objects.all()})
 
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        #form has been submitted, process data
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
